8ab2290a640214f91229bb096aaaaebd8c75e94c
nizarchaouch <chaouchnizar1@gmail.com>
2026-04-22 07:04:50 +0100

    feat: enhance UI layout and styling for Todo List component

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,22 @@ function App() {
   const remainingTasks = useMemo(() => tasks.filter((task) => !task.completed).length, [tasks]);
 
   return (
-    <div>
-      <h1>Todo List</h1>
-      <input type="text" placeholder="Add a new task..." />
-      <button>+</button>
-      <div>
+    //<div className="min-h-screen flex items-center justify-center bg-gray-100"></div>
+    <div className="max-w-xl mx-auto mt-10 p-4 bg-white flex flex-col gap-3 rounded-md shadow-md">
+      <h1 className="text-2xl font-bold mb-4 text-gray-700">Todo List</h1>
+      <div className="w-full flex gap-2" >
+        <input className="flex-1 border-b-2 border-r-2 border-gray-300 rounded-md py-1 px-2 mr-2 " type="text" placeholder="Add a new task..." />
+        <button className="bg-gray-700 rounded-md text-white py-1 px-2">+</button>
+      </div>
+      <div className="flex flex-col gap-3">
         {tasks.map((task) => <div key={task.id}>
-          <input type="checkbox" checked={task.completed} />
-          <span>{task.text}</span>
-          <button>x</button>
+          <div className="border-1 border-gray-200 p-2 rounded-md flex justify-between">
+            <div className=" flex items-center gap-2">
+              <input type="checkbox" checked={task.completed} />
+              <span>{task.text}</span>
+            </div>
+            <button>x</button>
+          </div>
         </div>)}
       </div>
       <div>Remaining tasks: {remainingTasks}</div>
