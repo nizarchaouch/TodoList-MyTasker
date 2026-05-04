c5188a0ef76e800a8b5ac85b132d8660199c465f
nizarchaouch <chaouchnizar1@gmail.com>
2026-05-04 19:09:38 +0100

    feat: enhance task UI with animated focus indicator and improved hover effects

--- src/App.tsx
+++ src/App.tsx
@@ -75,8 +75,8 @@ function App() {
   return (
     <div className="grid md:min-h-screen grid-cols-1 p-4 md:grid-cols-2 md:items-center">
       <div className="w-full  max-w-xl p-4 justify-self-end space-y-5 p-5">
-        <div className="inline-flex items-center bg-white/30  border border-gray-400  gap-2 rounded-full px-4 py-2 text-sm font-medium text-gray-600">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="stroke-orange-500 h-4 w-4 text-secondary"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"></path><path d="M20 3v4"></path><path d="M22 5h-4"></path><path d="M4 17v2"></path><path d="M5 18H3"></path></svg>
+        <div className="animate-bounce inline-flex items-center bg-white/30  border border-gray-400  gap-2 rounded-full px-4 py-2 text-sm font-medium text-gray-600">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="stroke-orange-500 h-4 w-4 text-secondary"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"></path><path d="M20 3v4"></path><path d="M22 5h-4"></path><path d="M4 17v2"></path><path d="M5 18H3"></path></svg>
           Daily focus board
         </div>
         <h1 className="max-w-xl text-5xl font-black leading-tight tracking-normal text-foreground sm:text-6xl">
@@ -125,12 +125,12 @@ function App() {
           ) : (
             tasks.map((task) => (
               <div key={task.id}>
-                <div className="flex justify-between border-1 border-gray-300 p-6 rounded-md bg-[#fdfaf2] transition-all hover:-translate-y-0.5 hover:border-primary/50 sm:gap-4 sm:p-4">
+                <div className="flex justify-between border-1 border-gray-300 p-6 rounded-md bg-[#fdfaf2] transition-all hover:-translate-y-0.5 hover:border-green-600 sm:gap-4 sm:p-4">
                   <div
                     className="w-full cursor-pointer"
                     onClick={() => handleTaskCompletion(task.id)}
                   >
-                    <div className=" flex  items-center gap-2">
+                    <div className="flex items-center gap-2">
                       <input
                         type="checkbox"
                         className="h-5 w-5 cursor-pointer appearance-none rounded-full border border-gray-400 checked:border-[#188174] checked:bg-[#188174]"
